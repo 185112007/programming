c5d798dc30289e1aeeac96bdf0e30536507b0067
Gafur Hayytbayev <185112007@kocaeli.edu.tr>
2020-05-13 15:48:47 +0300

    added ch09 examples of c prog book

---
+++ c/1_c_programming_a_modern_approach_2nd_edition/source/ch09/ex01/main.c
@@ -0,0 +1,26 @@
+/**
+* Name: main.c
+* Purpose: computes pairwise averages of three numbers
+* Autho: Gafur Hayytbayev
+*/
+
+#include <stdio.h>
+
+double average(double a, double b)
+{
+	return (a + b) / 2;
+}
+
+int main(void)
+{
+	double x, y, z;
+
+	printf("Enter three numbers: ");
+	scanf("%lf%lf%lf", &x, &y, &z);
+
+	printf("Average of %g and %g: %g\n", x, y, average(x, y));
+	printf("Average of %g and %g: %g\n", y, z, average(y, z));
+	printf("Average of %g and %g: %g\n", x, z, average(x, z));
+
+	return 0;
+}
---
+++ c/1_c_programming_a_modern_approach_2nd_edition/source/ch09/ex03/main.c
@@ -0,0 +1,18 @@
+/**
+* Name: main.c
+* Purpose: Prints a bad pun
+* Autho: Gafur Hayytbayev
+*/
+
+#include <stdio.h>
+
+void print_pun(void)
+{
+	printf("To C, or not to C: that is the question.\n");
+}
+
+int main(void)
+{
+	print_pun();
+	return 0;
+}
